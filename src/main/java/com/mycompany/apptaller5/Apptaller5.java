9cd739ec48beeb694532f47055d4bc86452c8419
Veritoo123 <112337712+Veritoo123@users.noreply.github.com>
2022-11-19 22:36:25 -0500

    Subiendo menus de clase

---
+++ src/main/java/com/mycompany/apptaller5/Apptaller5.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.apptaller5;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author Veronica
+ */
+public class Apptaller5 {
+
+    public static void main(String[] args) {
+        frmPrincipal principal= new frmPrincipal();
+        principal.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        principal.setVisible(true);
+    }
+}
---
+++ src/main/java/com/mycompany/apptaller5/Voltaje.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.apptaller5;
+
+/**
+ *
+ * @author Veronica
+ */
+public class Voltaje {
+    public static double calcular_voltaje(double corriente, double resistencia){
+        double voltaje;
+        voltaje= corriente + resistencia;
+        return voltaje;
+    }
+    
+}
